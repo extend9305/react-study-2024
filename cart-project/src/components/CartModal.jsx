af0ec3736035f20e8f6e34216390ffd81c726e05
dongsulee <extend9305@naver.com>
2024-01-28 13:36:32 +0900

    react Context and useReducer study 2024-01-28

---
+++ cart-project/src/components/CartModal.jsx
@@ -0,0 +1,34 @@
+import { forwardRef, useImperativeHandle, useRef ,useContext } from 'react';
+import { createPortal } from 'react-dom';
+import { CartContext } from '../store/shopping-cart-context';
+
+import Cart from './Cart';
+
+const CartModal = forwardRef(function Modal(
+  { title, actions },
+  ref
+) {
+  const dialog = useRef();
+  const { items ,updateItemToCart } = useContext(CartContext);
+
+  useImperativeHandle(ref, () => {
+    return {
+      open: () => {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return createPortal(
+    <dialog id="modal" ref={dialog}>
+      <h2>{title}</h2>
+      <Cart items={items} onUpdateItemQuantity={updateItemToCart} />
+      <form method="dialog" id="modal-actions">
+        {actions}
+      </form>
+    </dialog>,
+    document.getElementById('modal')
+  );
+});
+
+export default CartModal;
